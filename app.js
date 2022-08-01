8fdfe0c0bd0be11b491a85abc1450fdf3bdd1607
Sarah Alexa <sarahalexa@Sarahs-MBP.lan>
2022-08-01 12:55:54 +0100

    TD3 GET /api/topics endpoint added and EHMF

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+const { getTopics } = require('./controller/news.controller')
+
+app.get('/api/topics', getTopics)
+
+
+
+app.all('/*', (req, res) => {
+    res.status(404).send({msg: 'Route not found'})
+})
+///////////////////////////////////////////////////////
+
+app.use((err, req, res, next) => {
+    console.log('500 catch')
+    res.status(err.status).send({msg:err.msg});
+})
+
+module.exports = app
