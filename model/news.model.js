04f78d75ded7df677bcad740a690bd078cafaf10
Sarah Alexa <sarahalexa@Sarahs-MBP.lan>
2022-08-04 10:31:24 +0100

    Added TD11, sort_by and order working

--- model/news.model.js
+++ model/news.model.js
@@ -42,14 +42,32 @@ exports.selectUsers = () => {
   });
 };
 
-exports.selectArticles = () => {
-  return db
-    .query(
-      "SELECT articles.article_id, articles.title, articles.author, articles.topic, articles.created_at, articles.votes, COUNT(comment_id) AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id GROUP BY articles.article_id ORDER BY created_at DESC;"
-    )
-    .then(({ rows }) => {
-      return rows;
-    });
+exports.selectArticles = (sort_by = "created_at", order = "DESC") => {
+  const validSortBy = [
+    "article_id",
+    "title",
+    "author",
+    "topic",
+    "votes",
+    "created_at",
+    "comment_count",
+  ];
+  const validOrder = ["asc", "desc", "ASC", "DESC"];
+
+  if (!validSortBy.includes(sort_by)) {
+    return Promise.reject({ status: 400, msg: "Bad request" });
+  }
+  if (!validOrder.includes(order)) {
+    return Promise.reject({ status: 400, msg: "Bad request" });
+  }
+  let queryStr =
+    "SELECT articles.article_id, articles.title, articles.author, articles.topic, articles.created_at, articles.votes, COUNT(comment_id) AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id GROUP BY articles.article_id ";
+
+  queryStr += `ORDER BY ${sort_by} ${order};`;
+
+  return db.query(queryStr).then(({ rows }) => {
+    return rows;
+  });
 };
 
 exports.selectCommentsByArticleID = (article_id) => {
